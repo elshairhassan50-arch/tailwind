b0720dbf811beb0586bbf745eef83b3d1945dfa7
elshairhassan50-arch <elshairhassan50@gmail.com>
2026-04-13 20:06:24 +0200

    initial commit - empty components

---
+++ src/Components/AiTypes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AiTypes() {
+  return 
+    <>
+    
+    </>
+  
+}
+
+export default AiTypes
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import AiTypes from '../Components/AiTypes'
+import AiBenefits from '../Components/AiBenfits'
+import Contacts from '../Components/Contacts'
+import Footer from '../Components/Footer'
+import HeroSections from '../Components/HeroSections'
+import Navbar from '../Components/Navbar'
+
+function Home() {
+  return <>
+    <Navbar/>
+    <main>
+      <HeroSections/>
+      <AiTypes/>
+      <AiBenefits/>
+      <Contacts/>
+    </main>
+    <Footer/>
+   </>
+}
+
+export default Home
