107b7e6103a2c6b6e5bf61b149522b89aaa0e9cf
elshairhassan50-arch <elshairhassan50@gmail.com>
2026-04-13 21:49:48 +0200

    add hero section

--- src/Components/HeroSections.jsx
+++ src/Components/HeroSections.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
+const images = [
+  "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=600&auto=format&fit=crop&q=60",
+  "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=600&auto=format&fit=crop&q=60",
+  "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60",
+];
+
 
 function HeroSections() {
   return <>
-   <div>
-      
+  <section className='app-container pt-24 pb-12 overflow-hidden sm:overflow-visible'>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 items-center'>
+      <div className='space-y-6' >
+        <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold leading-tight'><span className='gradient-text'>Artificial Intelligence</span>
+        <br />
+        for a Smarter Future.
+         </h1>
+         <p className='text-lg text-gray-600'>
+           Discover how AI is transforming industries and improving lives worldwide. Our cutting-edge solutions harness the power of machine learning to solve complex problems.
+         </p>
+         <div className='flex flex-col sm:flex-row gap-4 pt-4'>
+            <a href='#contact' className='btn-primary w-full sm:w-auto text-center'>Explore AI Solutions</a>
+            <a href='#contact' className='btn-outline w-full sm:w-auto text-center'>Learn More</a>
+         </div>
+         <div className='pt-4 flex items-center space-x-4'>
+          <div className='flex -space-x-2'>
+            {images.map((img, i) => (
+              <img
+              className="avatar"
+                key={i}
+                src={img}
+                alt={`User ${i + 1}`}
+              />
+            ))}
+          </div>
+          <p className='text-smfont-medium text-gray-600'>Trusted by{" "}
+             <span className='text-primary font-bold'>10,000+</span> People WoldWide</p>
+         </div>
+      </div>
+      <div className='relative w-full h-80 md:h-[600px]'>
+        <img 
+        className='w-full h-full object-cover rounded-2xl'
+        src="https://images.unsplash.com/photo-1674027444485-cec3da58eef4?q=80&w=1932&auto=format&fit=crop"
+        alt="hero section" 
+        />
+        <div className='absolute w-32 h-32 rounded-full bg-secondary/20 -bottom-6 -left-6 blur-xl'></div>
+        <div className='absolute w-32 h-32 rounded-full bg-accent/20 -top-6 -right-6 blur-xl'></div>
+
+
+
+      </div>
     </div>
+
+  </section>
+   
   </>
    
   
